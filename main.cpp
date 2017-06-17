afbe9918a9932c5f6526ffecbd1048189e19f958
Dacoda Strack <dacoda.strack@gmail.com>
2017-06-17 11:36:04 -0700

    vibrate over time with gnuplot

--- main.cpp
+++ main.cpp
@@ -54,35 +54,42 @@ int main()
         }
     }
 
+    const double sphere_r = 5;
     std::vector<std::vector<double>> scales(lmax+1, std::vector<double>(lmax+1, 0.0));
-    scales[2][2] = 1;
+    scales[2][2] = 3;
 
     FILE *pipe = popen("gnuplot -persist", "w");
     fprintf(pipe, "set mapping spherical\n");
     fprintf(pipe, "set term qt noraise\n");
     fprintf(pipe, "set pm3d depthorder\n");
-    fprintf(pipe, "$data << EOD\n");
+    fprintf(pipe, "set cbrange [-1:1]\n");
+    fprintf(pipe, "set xrange [-8:8]\n");
+    fprintf(pipe, "set yrange [-8:8]\n");
+    fprintf(pipe, "set zrange [-8:8]\n");
 
-    const double sphere_r = 5;
-    // Calculate the actual value for the current time slice
-    for ( size_t idx_th = 0; idx_th < sample_points; ++idx_th )
-    {
-        for (size_t idx_ph = 0; idx_ph < sample_points; ++idx_ph )
-        {
-            double cur_val = 0;
-            for (size_t l = 0; l <= lmax; ++l)
-                for (size_t m = 0; m <= lmax; ++m)
-                {
-                    size_t idx_th_res = gsl_sf_legendre_array_index(l, m);
-                    cur_val += scales[l][m] * phi_result[idx_ph][m] * theta_result[idx_th][idx_th_res];
-                }
-            fprintf(pipe, "%.3f %.3f %.3f %.3f\n", theta[idx_th], phi[idx_ph], sphere_r + cur_val, cur_val);
+    double t = 0;
+    while (true) {
+        fprintf(pipe, "$data << EOD\n");
+
+        // Calculate the actual value for the current time slice
+        for (size_t idx_th = 0; idx_th < sample_points; ++idx_th) {
+            for (size_t idx_ph = 0; idx_ph < sample_points; ++idx_ph) {
+                double cur_val = 0;
+                for (size_t l = 0; l <= lmax; ++l)
+                    for (size_t m = 0; m <= lmax; ++m) {
+                        size_t idx_th_res = gsl_sf_legendre_array_index(l, m);
+                        cur_val += cos(4*t) * scales[l][m] * phi_result[idx_ph][m] * theta_result[idx_th][idx_th_res];
+                    }
+                fprintf(pipe, "%.3f %.3f %.3f %.3f\n", theta[idx_th], phi[idx_ph], sphere_r + cur_val, cur_val);
+            }
+            fprintf(pipe, "\n");
         }
-        fprintf(pipe, "\n");
-    }
 
-    fprintf(pipe, "EOD\n");
-    fprintf(pipe, "splot $data w pm3d\n");
+        fprintf(pipe, "EOD\n");
+        fprintf(pipe, "splot $data w pm3d\n");
+
+        t += .1;
+    }
 
     fclose(pipe);
 }
